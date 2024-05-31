ad53a4147de9b811c938b2fd7423da4017a82da7
Songhee120 <81075657+Songhee120@users.noreply.github.com>
2024-06-01 03:37:12 +0900

    feat: 내정보 조회

--- src/member/MyInfo.js
+++ src/member/MyInfo.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Style.css'; // CSS 파일을 import
-import { Link } from 'react-router-dom';
 import logoImage from '../img/semohan-logo.png';
+import axios from 'axios';
 
 function MyInfoPage() {
+    const [ownerInfo, setOwnerInfo] = useState({
+        name: '',
+        userName: '',
+        phoneNumber: ''
+    });
+
+    useEffect(() => {
+        // /restaurant/info로 GET 요청을 보내고 데이터를 받아옴
+        const fetchOwnerInfo = async () => {
+            try {
+                const response = await axios.get('/owner/info', {
+                    withCredentials: true
+                });
+                setOwnerInfo(response.data);
+            } catch (error) {
+                console.error('Error fetching owner info:', error);
+            }
+        };
+
+        fetchOwnerInfo();
+    }, []);
+
+
     return (
         <div id="body">
             <header>
-                <img src={logoImage} alt="logo"/>
+                <img src={logoImage} alt="logo" />
             </header>
             <table>
                 <caption>나의 정보</caption>
                 <tbody>
-                    <tr>
-                        <th>이름</th>
-                        <td>세모한{/* 여기에 이름 데이터를 넣으세요 */}</td>
-                    </tr>
-                    <tr>
-                        <th>아이디</th>
-                        <td>semohan123{/* 여기에 아이디 데이터를 넣으세요 */}</td>
-                    </tr>
-                    <tr>
-                        <th>비밀번호</th>
-                        <td>12345{/* 여기에 비밀번호 데이터를 넣으세요 */}</td>
-                    </tr>
+                <tr>
+                    <th>이름</th>
+                    <td>{ownerInfo.name}</td>
+                </tr>
+                <tr>
+                    <th>아이디</th>
+                    <td>{ownerInfo.userName}</td>
+                </tr>
+                <tr>
+                    <th>전화번호</th>
+                    <td>{ownerInfo.phoneNumber}</td>
+                </tr>
                 </tbody>
             </table>
             <input className="submit" type="button" value="정보 수정" />
