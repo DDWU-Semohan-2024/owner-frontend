e234ffc303f7af95356052949cb2c3f744aa1961
Songhee120 <81075657+Songhee120@users.noreply.github.com>
2024-06-06 13:44:01 +0900

    feat: 로그아웃, 아이디찾기-인증번호전송

--- src/member/FindId.js
+++ src/member/FindId.js
@@ -3,10 +3,24 @@ import './Style.css';
 // import { Link } from 'react-router-dom';
 import logoImage from '../img/semohan-logo.png';
 import {Link} from "react-router-dom";
+import axios from 'axios';
 
 function FindId() {
-    const [phoneNum, setPhoneNum] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
     const [certificationNum, setCertificationNum] = useState('');
+
+    const handleSendCertification = async () => {
+        try {
+            const response = await axios.post('/auth/find-id/send', {phoneNumber}.phoneNumber);
+            if (response.status === 200) {
+                // Handle success response
+                console.log('Certification number sent successfully');
+            }
+        } catch (error) {
+            // Handle error response
+            console.error('Failed to send certification number', error);
+        }
+    };
 
     return (
         <div id="body">
@@ -14,19 +28,19 @@ function FindId() {
                 <img src={logoImage} alt="logo"/>
             </header>
             <form id="findId" action="" method="post">
-                <label htmlFor="phoneNum">휴대전화</label>
+                <label htmlFor="phoneNumber">휴대전화</label>
                 <div className="certification">
-                    <input className="blank" type="tel" name="phoneNum" id="phoneNum" autoComplete="tel"
-                           value={phoneNum}
-                           onChange={(e) => setPhoneNum(e.target.value)}/>
-                    <input className="certi" type="button" value="인증번호"/>
+                    <input className="blank" type="tel" name="phoneNumber" id="phoneNumber" autoComplete="tel"
+                           value={phoneNumber}
+                           onChange={(e) => setPhoneNumber(e.target.value)}/>
+                    <input className="certi" type="button" value="인증번호" onClick={handleSendCertification}/>
                 </div>
                 <div className="certification">
                     <input className="blank" type="number" name="certificationNum"
                            placeholder="인증번호를 입력하세요" id="certiPhone"
                            value={certificationNum}
                            onChange={(e) => setCertificationNum(e.target.value)}/>
-                    <input className="certi" type="button" value="확인"/>
+                    <input className="certi" type="button" value="인증확인"/>
                 </div>
                 <div className="find">
                     <Link to="/resultId">아이디 찾기</Link>
--- src/member/Main.js
+++ src/member/Main.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Style.css'; // CSS 파일을 import
-import { Link } from 'react-router-dom';
+import { Link, } from 'react-router-dom';
+import axios from 'axios';
 import logoImage from '../img/semohan-logo.png';
 
+axios.defaults.withCredentials = true;
+
 function Main() {
+
+    const handleLogout = async () => {
+        try {
+            await axios.post('/auth/sign-out');
+            window.location.href = '/login';
+        } catch (error) {
+            console.error('There was an error logging out!', error);
+        }
+    };
 
     return (
         <div id="body">
@@ -15,7 +27,7 @@ function Main() {
                 <Link className="gray" to="/restaurantInfo">식당 정보</Link>
                 <Link className="lemon" to="/registerMenu">메뉴 등록</Link>
                 <Link className="gray" to="/">비즈니스</Link>
-                <Link className="gray" to="/login">로그아웃</Link>
+                <Link className="gray" onClick={handleLogout}>로그아웃</Link>
             </div>
         </div>
     );
