e2196a9af2b5bb26e2c6becde8b4e43cd94658c6
Songhee120 <81075657+Songhee120@users.noreply.github.com>
2024-10-14 18:01:58 +0900

    update: strictMode 삭제

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 axios.defaults.baseURL = "http://localhost:8088"
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <App />
-    </React.StrictMode>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
